5ad198027213fcfbab4bc97f443b7a169ca3a4c9
murillojulio <juarmuri@gmail.com>
2020-04-28 00:22:18 -0500

    Se crea la clase MainEvent que sirve como comunicacion unica del modulo

---
+++ app/src/main/java/com/murillojulio/inventario/mainModule/events/MainEvent.java
@@ -0,0 +1,48 @@
+package com.murillojulio.inventario.mainModule.events;
+/*
+* Esta clase nos sirve como comunicacion unica del modulo:
+* Transportara:
+* product
+* typeEvent(add, update, remove)
+* resMsg*/
+
+import com.murillojulio.inventario.common.pojo.Product;
+
+public class MainEvent {
+    public static final int SUCCESS_ADD = 0;
+    public static final int SUCCESS_UPDATE = 1;
+    public static final int SUCCESS_REMOVE = 2;
+    public static final int ERROR_SERVER = 100;
+    public static final int ERROR_TO_REMOVE = 101;
+
+    private Product product;
+    private int typeEvent;
+    private int resMsg;
+
+    public MainEvent() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getTypeEvent() {
+        return typeEvent;
+    }
+
+    public void setTypeEvent(int typeEvent) {
+        this.typeEvent = typeEvent;
+    }
+
+    public int getResMsg() {
+        return resMsg;
+    }
+
+    public void setResMsg(int resMsg) {
+        this.resMsg = resMsg;
+    }
+}
